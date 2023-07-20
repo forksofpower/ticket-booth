13c43950f824672dc837db3c3dfa06fcd56fd67e
Patrick Jones <patrickjones.pmj@gmail.com>
2023-07-20 18:43:20 -0500

    add mongoose, mongodb deployment/service, and modify auth to connect to mongodb

--- apps/auth/src/index.ts
+++ apps/auth/src/index.ts
@@ -1,7 +1,13 @@
+// 3rd Party
 import express from "express";
 import "express-async-errors";
-import { routes } from "./routes/";
+import mongoose from "mongoose";
+
+// Middleware
 import { errorHandler } from "./middleware/error-handler";
+import { routes } from "./routes/";
+
+// Errors
 import { NotFoundError } from "./errors/not-found-error";
 
 const port = process.env.PORT || 4000;
@@ -17,6 +23,17 @@ app.all("*", async (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+  });
+};
+
+start();
