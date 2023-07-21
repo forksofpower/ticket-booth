8613e83e1c57b39d424dc5bf84be8d9220f1fb90
Patrick Jones <patrickjones.pmj@gmail.com>
2023-07-20 19:57:55 -0500

    add BadRequestError

---
+++ apps/auth/src/errors/bad-request-error.ts
@@ -0,0 +1,15 @@
+import { CustomError, SerializedError } from "./custom-error";
+
+export class BadRequestError extends CustomError {
+  statusCode = 400;
+
+  constructor(public message: string) {
+    super(message);
+
+    Object.setPrototypeOf(this, BadRequestError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.message }];
+  }
+}
