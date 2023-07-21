c243aca388210a71261abf4fd3ca5bf12b0a3d58
Patrick Jones <patrickjones.pmj@gmail.com>
2023-07-21 02:45:57 -0500

    update server

--- apps/auth/src/index.ts
+++ apps/auth/src/index.ts
@@ -2,6 +2,7 @@
 import express from "express";
 import "express-async-errors";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 // Middleware
 import { errorHandler } from "./middleware/error-handler";
@@ -9,21 +10,36 @@ import { routes } from "./routes/";
 
 // Errors
 import { NotFoundError } from "./errors/not-found-error";
+import { config } from "./config";
+import { currentUser } from "./middleware/current-user";
 
 const port = process.env.PORT || 4000;
 const app = express();
 
+// Allow traffic from nginx proxy
+app.set("trust proxy", true);
+// Request JSON body support
 app.use(express.json());
-
+// Configure Session with cookie support
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+// Configure Routes
 app.use(routes);
-
+// Configure Catch-All Route
 app.all("*", async (req, res) => {
   throw new NotFoundError();
 });
-
+// Configure Error Handling
 app.use(errorHandler);
-
+// Configure Server Start
 const start = async () => {
+  if (!config.jwtSecret) {
+    throw new Error("JWT_SECRET is undefined");
+  }
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
     console.log("Connected to MongoDB");
@@ -35,5 +51,4 @@ const start = async () => {
     console.log(`listening on port ${port}`);
   });
 };
-
 start();
