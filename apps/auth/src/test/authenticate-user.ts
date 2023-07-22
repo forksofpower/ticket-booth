7f95f627da583dfc453e077eb1a54e11af0510f7
Patrick Jones <patrickjones.pmj@gmail.com>
2023-07-22 00:20:52 -0500

    add helper to automate authenticating a user in tests

---
+++ apps/auth/src/test/authenticate-user.ts
@@ -0,0 +1,19 @@
+import request from "supertest";
+import { app } from "../app";
+/**
+ * Helper function to signup and return cookie
+ * @returns Promise<string[]> - Cookie array
+ */
+export const authenticateUser = async () => {
+  const email = "test@testing.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({ email, password })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+
+  return cookie;
+};
