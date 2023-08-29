b71bf31e97ffe667d4c8e33afdc5f6af67c6e3df
Patrick Jones <patrickjones.pmj@gmail.com>
2023-08-29 00:36:20 -0500

    add buildClient for use in getInitialProps

---
+++ apps/client/src/utils/build-client.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { NextPageContext } from "next";
+
+import { isServer } from "./predicates";
+
+const buildClient = ({ req }: NextPageContext) => {
+  if (isServer) {
+    // We are on the server
+    // Requests should be made to http://SERVICENAME.NAMESPACE.svc.cluster.local/ROUTE
+    return axios.create({
+      baseURL:
+        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      headers: req!.headers,
+    });
+  } else {
+    // We are on the browser
+    // Requests can be made with a base url of ''
+    return axios.create({
+      baseURL: "/",
+      withCredentials: true,
+    });
+  }
+};
+
+export default buildClient;
