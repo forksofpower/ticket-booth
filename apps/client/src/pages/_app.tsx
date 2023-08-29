4cebf5825d239041fe5dc41ce2edac9a30981e3f
Patrick Jones <patrickjones.pmj@gmail.com>
2023-08-29 00:47:42 -0500

    setup current user provider in _app

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -2,10 +2,13 @@ import "@/styles/globals.css";
 
 import { NextPage } from "next";
 
+import { CurrentUserProvider } from "@/contexts/current-user";
+import { User } from "@/types/user";
+import buildClient from "@/utils/build-client";
+
 // import ThemeToggle from "@/components/theme-toggle";
 
-import type { AppProps } from "next/app";
-
+import type { AppContext, AppProps } from "next/app";
 export type NextPageWithLayout<Props = {}, InitialProps = Props> = NextPage<
   Props,
   InitialProps
@@ -15,12 +18,33 @@ export type NextPageWithLayout<Props = {}, InitialProps = Props> = NextPage<
 
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
+  currentUser: User | null;
 };
 
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
-  // useEffect(() => {
-  //   document.querySelector("html")?.setAttribute("data-theme", "cyberpunk");
-  // }, []);
+function App({ Component, pageProps, currentUser }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page);
-  return getLayout(<Component {...pageProps} />);
+
+  return (
+    <CurrentUserProvider currentUser={currentUser}>
+      {getLayout(<Component {...pageProps} />)}
+    </CurrentUserProvider>
+  );
 }
+
+App.getInitialProps = async (context: AppContext) => {
+  const client = buildClient(context.ctx);
+
+  const {
+    data: { currentUser },
+  } = await client.get<{ currentUser: User }>(`/api/users/currentuser`);
+
+  const pageProps =
+    (await context.Component.getInitialProps?.(context.ctx)) || {};
+
+  return {
+    pageProps,
+    currentUser,
+  };
+};
+
+export default App;
