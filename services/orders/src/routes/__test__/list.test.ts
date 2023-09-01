deda4ea784eda43ef3db8902d37755819811c423
Patrick Jones <patrickjones.pmj@gmail.com>
2023-09-01 14:21:45 -0500

    list orders route

--- services/orders/src/routes/__test__/list.test.ts
+++ services/orders/src/routes/__test__/list.test.ts
@@ -2,5 +2,46 @@ import request from "supertest";
 
 import { app } from "../../app";
 import { authenticateUser } from "../../test/authenticate-user";
+import { buildTicket } from "../../test/build-ticket";
 
-describe("Ticket: List", () => {});
+describe("Ticket: List", () => {
+  it("fetches orders for a particular user", async () => {
+    // create three tickets
+    const ticketOne = await buildTicket();
+    const ticketTwo = await buildTicket();
+    const ticketThree = await buildTicket();
+
+    // create one order as user1
+    const user1 = authenticateUser();
+    const { body: orderOne } = await request(app)
+      .post("/api/orders")
+      .set("Cookie", user1)
+      .send({ ticketId: ticketOne.id })
+      .expect(201);
+
+    // create two orders as user2
+    const user2 = authenticateUser();
+    const { body: orderTwo } = await request(app)
+      .post("/api/orders")
+      .set("Cookie", user2)
+      .send({ ticketId: ticketTwo.id })
+      .expect(201);
+    const { body: orderThree } = await request(app)
+      .post("/api/orders")
+      .set("Cookie", user2)
+      .send({ ticketId: ticketThree.id })
+      .expect(201);
+
+    // make request to get orders for user2
+    const response = await request(app)
+      .get("/api/orders")
+      .set("Cookie", user2)
+      .expect(200);
+
+    // Expect user2 orders to not include user1 orders
+    expect(response.body.length).toEqual(2);
+    expect(response.body.at(0).id).toEqual(orderTwo.id);
+    expect(response.body.at(1).id).toEqual(orderThree.id);
+    expect(response.body).not.toContain(orderOne);
+  });
+});
--- services/orders/src/routes/list.ts
+++ services/orders/src/routes/list.ts
@@ -1,12 +1,17 @@
 import express, { Request, Response } from "express";
 
-import { NotImplementedError } from "@forksofpower/ticketbooth-common";
+import { OrderStatus, requireAuth } from "@forksofpower/ticketbooth-common";
 
-// import { Order } from "../models/order";
+import { Order } from "../models/order";
 
 const router = express.Router();
-router.get("/api/tickets", async (req: Request, res: Response) => {
-  throw new NotImplementedError();
+router.get("/api/orders", requireAuth, async (req: Request, res: Response) => {
+  const orders = await Order.find({
+    userId: req.currentUser!.id,
+    status: { $ne: OrderStatus.Cancelled },
+  }).populate("ticket");
+
+  res.send(orders);
 });
 
 export { router as listOrderRouter };
