7c2453fde6049cbb16560fff4f62fab4f0e3b682
Patrick Jones <patrickjones.pmj@gmail.com>
2023-09-01 14:21:53 -0500

    show order route

--- services/orders/src/routes/__test__/show.test.ts
+++ services/orders/src/routes/__test__/show.test.ts
@@ -2,6 +2,32 @@ import mongoose from "mongoose";
 import request from "supertest";
 
 import { app } from "../../app";
+import { Order, OrderStatus } from "../../models/order";
 import { authenticateUser } from "../../test/authenticate-user";
+import { buildTicket } from "../../test/build-ticket";
 
-describe("Ticket: Show", () => {});
+describe("Order: Show", () => {
+  it("returns a 404 if the ticket is not found", async () => {
+    const orderId = new mongoose.Types.ObjectId().toHexString();
+
+    await request(app)
+      .get(`/api/orders/${orderId}`)
+      .set("Cookie", authenticateUser())
+      .expect(404);
+  });
+  it("returns the ticket if the ticket is found", async () => {
+    const ticket = await buildTicket();
+
+    const user = authenticateUser();
+    const { body: order } = await request(app)
+      .post("/api/orders")
+      .set("Cookie", user)
+      .send({ ticketId: ticket.id })
+      .expect(201);
+
+    await request(app)
+      .get(`/api/orders/${order.id}`)
+      .set("Cookie", user)
+      .expect(200);
+  });
+});
--- services/orders/src/routes/show.ts
+++ services/orders/src/routes/show.ts
@@ -1,14 +1,38 @@
 import express, { Request, Response } from "express";
+import { param } from "express-validator";
 
-import { NotImplementedError } from "@forksofpower/ticketbooth-common";
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  NotImplementedError,
+  requireAuth,
+  validateRequest,
+} from "@forksofpower/ticketbooth-common";
+
+import { Order } from "../models/order";
 
 // import { NotFoundError } from "@forksofpower/ticketbooth-common";
 
 // import { Order } from "../models/order";
 
 const router = express.Router();
-router.get("/api/orders/:id", async (req: Request, res: Response) => {
-  throw new NotImplementedError();
-});
+router.get(
+  "/api/orders/:id",
+  requireAuth,
+  [
+    param("id")
+      .notEmpty()
+      .isMongoId()
+      .withMessage("correctly formatted Ticket ID must be provided"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.id);
+    if (!order) throw new NotFoundError();
+    if (order.userId !== req.currentUser!.id) throw new NotAuthorizedError();
+
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
