5139385d8cc609477f1306a6cb574e078bc85b78
Patrick Jones <patrickjones.pmj@gmail.com>
2023-08-31 23:43:20 -0500

    test passthrough for getEnv

--- lib/common/src/utils/config.ts
+++ lib/common/src/utils/config.ts
@@ -1,6 +1,9 @@
 // get environment variable or die trying
 export function getEnv(key: string) {
+  if (process.env.NODE_ENV === "test") return "test";
+
   const value = process.env[key];
+
   if (!value) {
     throw new Error(`${key} must be defined`);
   }
