f33849d3b72233dcea84d916941445a3088a6947
Patrick Jones <patrickjones.pmj@gmail.com>
2023-08-31 21:14:24 -0500

    update errors export in common

--- lib/common/src/errors/index.ts
+++ lib/common/src/errors/index.ts
@@ -3,3 +3,4 @@ export { DatabaseConnectionError } from "./database-connection-error";
 export { BadRequestError } from "./bad-request-error";
 export { NotFoundError } from "./not-found-error";
 export { NotAuthorizedError } from "./not-authorized-error";
+export { NotImplementedError } from "./not-implemented-error";
--- services/orders/src/routes/new.ts
+++ services/orders/src/routes/new.ts
@@ -11,7 +11,7 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.post("/api/orders", async (req: Request, res: Response) => {
-  res.status(501).send("Not implemented");
+  throw new NotImple();
 });
 
 export { router as createOrderRouter };
