3d502188b0772a1c0d830b9f4dffcd128b39b8b8
Patrick Jones <patrickjones.pmj@gmail.com>
2023-08-31 23:02:04 -0500

    fix

--- lib/common/src/events/index.ts
+++ lib/common/src/events/index.ts
@@ -1,2 +1,3 @@
+export * from "./types";
 export * from "./ticket-created";
 export * from "./ticket-updated";
--- lib/common/src/index.ts
+++ lib/common/src/index.ts
@@ -1,5 +1,4 @@
 export * from "./errors";
 export * from "./middleware";
 export * from "./events";
-export * from "./types";
 export * from "./utils";
