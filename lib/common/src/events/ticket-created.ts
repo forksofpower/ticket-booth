36693e2d24bf7e451304161899517effd1970f0c
Patrick Jones <patrickjones.pmj@gmail.com>
2023-09-05 16:11:05 -0500

    refactor events

--- lib/common/src/events/ticket-created.ts
+++ lib/common/src/events/ticket-created.ts
@@ -2,8 +2,7 @@ import { Message } from "node-nats-streaming";
 
 import { Listener, Publisher } from "./base";
 import { Subjects } from "./subjects";
-
-import type { DomainEvent } from "./types";
+import { DomainEvent } from "./types";
 
 /**
  * Event
@@ -24,23 +23,4 @@ export type TicketCreatedEvent = DomainEvent<
  */
 export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
   readonly subject = Subjects.TicketCreated;
-}
-
-/**
- * Listener
- */
-export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
-  readonly subject = Subjects.TicketCreated;
-  queueGroupName = "payments-service";
-
-  onMessage(data: TicketCreatedEventData, msg: Message): void {
-    console.log("Event Data:", {
-      id: data.id,
-      title: data.title,
-      price: data.price,
-    });
-
-    // successful message
-    msg.ack();
-  }
 }
