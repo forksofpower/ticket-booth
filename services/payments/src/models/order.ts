040dd3f6e1c0fbd7768b7fc88dadf078dc749ab4
Patrick Jones <patrickjones.pmj@gmail.com>
2023-09-07 15:54:33 -0500

    add order model to payments service

---
+++ services/payments/src/models/order.ts
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
+
+import { OrderStatus, Versionable } from "@forksofpower/ticketbooth-common";
+
+export { OrderStatus };
+
+// Types
+interface OrderAttrs extends Versionable {
+  id: string;
+  userId: string;
+  status: OrderStatus;
+  price: number;
+}
+interface OrderDoc extends mongoose.Document {
+  status: OrderStatus;
+  userId: string;
+  price: number;
+  version: number;
+}
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+// Schema
+const orderSchema = new mongoose.Schema(
+  {
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    // This really should be on a View and not on the Model
+    toJSON: {
+      transform(_doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+      versionKey: false,
+    },
+  }
+);
+// Enable OCC
+orderSchema.set("versionKey", "version");
+orderSchema.plugin(updateIfCurrentPlugin);
+
+// Model Static Methods
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order(attrs);
+};
+
+export const Order = mongoose.model<OrderDoc, OrderModel>("Order", orderSchema);
