5a029826dc1412b9a5d067465be5f0b1bcfadd92
Patrick Jones <patrickjones.pmj@gmail.com>
2023-09-22 22:41:47 -0500

    move useAuth logic to SignIn page, remove from form

--- services/client/src/pages/auth/signin.tsx
+++ services/client/src/pages/auth/signin.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 
-import SignInUserForm from "@/components/forms/signin-user-form";
+import SignInUserForm, { SignInUserFormInput } from "@/components/forms/signin-user-form";
 import AuthLayout from "@/components/layouts/auth-layout";
+import useAuth from "@/hooks/use-auth";
+import { normalizeErrorResponsesByField } from "@/utils/errors";
 
 import { NextPageWithLayout } from "../_app";
 
 const SigninPage: NextPageWithLayout = () => {
+  const { signIn, signInErrors: responseErrors } = useAuth();
+
+  async function onSubmit(data: SignInUserFormInput) {
+    await signIn(data);
+  }
+
   return (
     <>
-      <SignInUserForm />
+      <SignInUserForm
+        onSubmit={onSubmit}
+        fieldErrors={normalizeErrorResponsesByField(responseErrors || [])}
+      />
     </>
   );
 };
