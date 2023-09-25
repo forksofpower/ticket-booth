142b9eaa5d9434a1d7a0e57257a35a2a41baf69a
Patrick Jones <patrickjones.pmj@gmail.com>
2023-09-24 21:19:07 -0500

    add config

--- services/client/src/utils/build-client.ts
+++ services/client/src/utils/build-client.ts
@@ -1,15 +1,18 @@
 import axios from "axios";
 import { IncomingHttpHeaders } from "http";
 
+import { config } from "@/config";
+
 import { isServer } from "./predicates";
 
-const buildClient = (requestHeaders: IncomingHttpHeaders) => {
+const { apiGatewayUrl } = config;
+
+const buildClient = (requestHeaders?: IncomingHttpHeaders) => {
   if (isServer) {
     // We are on the server
     // Requests should be made to http://SERVICENAME.NAMESPACE.svc.cluster.local/ROUTE
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: apiGatewayUrl,
       headers: requestHeaders,
     });
   } else {
