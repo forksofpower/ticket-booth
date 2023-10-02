c27e272d639493b09a17ef7efd818606ee195b8c
Patrick Jones <patrickjones.pmj@gmail.com>
2023-10-02 04:04:43 -0500

    proxy handler for api calls with revalidation

---
+++ services/client/src/utils/route-handler.ts
@@ -0,0 +1,32 @@
+import { revalidatePath } from "next/cache";
+import { NextRequest } from "next/server";
+
+import config from "@/config";
+
+export async function requestHandler(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams;
+  const revalidate = searchParams.get("revalidate");
+  const body = req.body;
+  const url = `${config.apiGatewayUrl}${req.nextUrl.pathname}`;
+
+  const payload = {
+    method: req.method,
+    headers: req.headers,
+    ...(req.body ? { body: JSON.stringify(body) } : {}),
+  };
+
+  const response = await fetch(url, payload);
+
+  if (response.ok && revalidate) {
+    try {
+      revalidatePath(revalidate);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers: response.headers,
+  });
+}
